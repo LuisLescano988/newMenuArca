cf2eda7e739d310fb02295ae2eb92a0004c36b74
Luis Lescano <luislescanologo@hotmail.com>
2024-07-22 00:31:19 -0300

    vinos & wines chart refresh 21-07-2024

--- src/views/Vinos.jsx
+++ src/views/Vinos.jsx
@@ -3,6 +3,10 @@ import img1 from '../assets/menus/vinos/vinos1.jpg'
 import img2 from '../assets/menus/vinos/vinos2.jpg'
 import img3 from '../assets/menus/vinos/vinos3.jpg'
 import img4 from '../assets/menus/vinos/vinos4.jpg'
+import img5 from '../assets/menus/vinos/vinos5.jpg'
+import img6 from '../assets/menus/vinos/vinos6.jpg'
+import img7 from '../assets/menus/vinos/vinos7.jpg'
+import img8 from '../assets/menus/vinos/vinos8.jpg'
 import { TiArrowBackOutline } from 'react-icons/ti'
 import { NavLink } from 'react-router-dom'
 
@@ -18,6 +22,10 @@ const Vinos = () => {
             <img className=' md:h-full md:m-auto' src={img2} alt="" />
             <img className=' md:h-full md:m-auto' src={img3} alt="" />
             <img className=' md:h-full md:m-auto' src={img4} alt="" />
+            <img className=' md:h-full md:m-auto' src={img5} alt="" />
+            <img className=' md:h-full md:m-auto' src={img6} alt="" />
+            <img className=' md:h-full md:m-auto' src={img7} alt="" />
+            <img className=' md:h-full md:m-auto' src={img8} alt="" />
     </div>
   )
 }
