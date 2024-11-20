ade1e710d978cc7ddc71685ab73d84757ad70ea4
LuisLescano988 <luislescanologo@hotmail.com>
2024-11-19 23:05:46 -0300

    replace jps by react 1

---
+++ src/data/menuData.js
@@ -0,0 +1,310 @@
+// data/menuData.js
+export const menuData = {
+  carte: [
+    {
+      name: "DZIKILPAK",
+      description: "smoked pumpkin seeds - chive oil - heirloom coriander - xoconostle crispy heirloom corn tostada",
+      price: "520",
+      isVegetarian: true
+    },
+    {
+      name: "SCALLOP CRUDO",
+      description: "smoked pumpkin seeds - chive oil - heirloom coriander - xoconostle crispy heirloom corn tostada\nadd caviar + 1200",
+      price: "850"
+    },
+    {
+      name: "BLUEFIN TUNA CRUDO",
+      description: "hibiscus and fermented plum aguachile - morita chili - dulce seaweed\npurple basil",
+      price: "780"
+    },
+    {
+      name: "VENISSON TARTARE",
+      description: "smoked salsa macha - jícama - cotixa cheese - blood tostadas",
+      price: "660"
+    },
+    {
+      name: "GRILLED BROCCOLINI",
+      description: "herb cesar dressing - fried capers - crispy bread - parmesan crisps\ntoasted seaweed",
+      price: "550",
+      isVegetarian: true
+    },
+    {
+      name: "SOFTSHELL CRAB TACO",
+      description: "amaranth and mezcal tempura - hoja santa emulsion - fermented habanero & xoconostle salsa",
+      price: "600"
+    },
+    {
+      name: "CRAB ESQUITES",
+      description: "heirloom corn - crab meat - coconut crab bisque - charred heirloom\ntomato - chives",
+      price: "700"
+    },
+    {
+      name: "ROASTED PRAWNS",
+      description: "morita chili - plantain vinegar - prawn butter - manzano chili ferment",
+      price: "700"
+    },
+    {
+      name: "SMOKED TROUT TOSTADA",
+      description: "crispy skin - trout roe - green allspice leaf sauce emulsion\npickled goose berrys - smoked crema",
+      price: "800"
+    },
+    {
+      name: "STRIPPED SEABASS",
+      description: "seared in coconut oil - green mole - local basil - coconut & jicama slaw",
+      price: "800"
+    },
+    {
+      name: "THE WHOLE SQUASH",
+      description: "roasted local squash - quash flowers - grilled squash leafs\ncrispy squash seed",
+      price: "580",
+      isVegetarian: true
+    },
+    {
+      name: "MAYAN OCTOPUS",
+      description: "lentil pure - lentil granola - pickled radishes - longaniza\nxcatic & allspice leaf salsa",
+      price: "780"
+    },
+    {
+      name: "SUCKLING PIG",
+      description: "crispy belly - smoked carnitas - cinnamon leaf oil - rice bean\nsoursop & sea purslane salsa",
+      price: "850"
+    },
+    {
+      name: "BONE MARROW",
+      description: "toasted in the woodfire oven - tamarind and pasilla glace\ngrilled pulque sourdough - serrano salsa",
+      price: "680"
+    },
+    {
+      name: "GRILLED MUSHROOM TLAYUDA",
+      description: "mushroom chicharrón - huitlacoche mole - chives - coriander\nblack sesame seed - nasturtium",
+      price: "550"
+    },
+    {
+      name: "MEXICAN WAGYU TLAYUDA",
+      description: "grilled flank steak - oxtail carnitas - smoked yuca pure - huilacoche mole",
+      price: "900"
+    },
+    {
+      name: "GRILLED SOURDOUGH PULQUE BREAD",
+      description: "coconut butter",
+      price: "280",
+      isVegetarian: true
+    },
+    {
+      name: "COCONUT FLOUR TORTILLA",
+      description: "",
+      price: "280",
+      isVegetarian: true
+    }
+  ],
+
+  carta: [
+    {
+      name: "DZIKILPAK",
+      description: "Dip de pepitas ahumadas - miso de pepitas - flor de calabaza - cilantro criollo - aceite de cebollín - tostadas tatemadas",
+      price: "520",
+      isVegetarian: true
+    },
+    {
+      name: "CRUDO DE CALLO DE HACHA",
+      description: "Aguachile de serrano y tomatillo fermentado - aceite de sacate limón - polvo de lechuga de mar - salicornia - chicharrón de callo y amaranto - kosho de lima local - aguacate y pepino parrillado",
+      price: "850"
+    },
+    {
+      name: "CRUDO DE ATÚN ALETA AZUL",
+      description: "Aguachile de chile morita y ciruela local fermentada - kosho de jamaica - oxalis - toronja - rábano sandía lacto fermentado - tostadas de tapioca",
+      price: "780"
+    },
+    {
+      name: "TARTAR VENADO LOCAL",
+      description: "Salsa macha - jícama - queso cotixa - chaya parrillada - tostadas tatemadas",
+      price: "660"
+    },
+    {
+      name: "BROCCOLINI PARRILLADO",
+      description: "Aderezo césar - suero de parmesano - cebollín - parmesano crujiente - alcaparras fritas",
+      price: "550",
+      isVegetarian: true
+    },
+    {
+      name: "TACOS DE JAIBA CONCHA SUAVE",
+      description: "En témpura de amaranto y mezcal - kosho de xoconostle y habanero - emulsión de hoja santa - hoja santa parrillada",
+      price: "600"
+    },
+    {
+      name: "CAMARONES DE CAMPECHE",
+      description: "Rostisados en mantequilla de chile morita - vinagre de plátano macho - salsa de chile manzano",
+      price: "700"
+    },
+    {
+      name: "ESQUITES DE CANGREJO",
+      description: "Maíz cacahuazintle - carne de cangrejo ahumada - bisque cangrejo y coco - polvo de flamboyan - maíz tostado - chorizo",
+      price: "700"
+    },
+    {
+      name: "TOSTADA DE TRUCHA AHUMADA",
+      description: "En hojas de pimiento gorda - chicharrón de piel de trucha - emulsión de aceite de hojas verdes de pimienta gorda - grosella fermentada - cilantro",
+      price: "800"
+    },
+    {
+      name: "LOBINA RAYADA",
+      description: "Sellada en aceite de coco - mole verde caribeño - ensalada de coco y jícama - serrano - aceite de albahaca local",
+      price: "800"
+    },
+    {
+      name: "CALABAZA LOCAL",
+      description: "A la parrilla - témpura de flores de calabaza - ensalada de hojas de calabaza y guías - mole de semillas de calabaza - granola de pepitas tostadas",
+      price: "580",
+      isVegetarian: true
+    },
+    {
+      name: "PULPO AL PASTOR",
+      description: "Cocinado en tepache - adobo de guajillo - mole de lentejas - granola de lentejas - rábanos encurtidos - salsa de chile xcatix y pimienta gorda",
+      price: "780"
+    },
+    {
+      name: "CARNITAS DE LECHÓN",
+      description: "Ahumadas en madera de canela fresca - reducción de orejón - verdogala - vinagre de mucilago de cacao local - aceite de hojas verdes de canela - tortillas",
+      price: "850"
+    },
+    {
+      name: "TUÉTANO ROSTIZADO",
+      description: "Rostizado al horno de leña - glaseado de tamarindo y pasilla - chile serrano - pan de pulque a la parrilla",
+      price: "680"
+    },
+    {
+      name: "TLAYUDA DE SETAS",
+      description: "Setas parrilladas - chicharrón de hongos - mole de huitlacoche cebollón - sésamo negro - mastuerzo",
+      price: "550"
+    },
+    {
+      name: "TLAYUDA DE WAGUY MEXICANO",
+      description: "Sésamo negro - mole de huitlacoche y tuétano - queso chiapas - carnitas de rabo de toro",
+      price: "900"
+    },
+    {
+      name: "PAN DE PULQUE",
+      description: "mantequilla de coco",
+      price: "280",
+      isVegetarian: true
+    },
+    {
+      name: "TORTILLAS",
+      description: "de harina",
+      price: "280",
+      isVegetarian: true
+    }
+  ],
+
+  beverages:[
+      {
+        name: "Agua De Mar Citrus Pale Ale",
+        price: "220"
+      },
+      {
+        name: "Loba Lager",
+        price: "180"
+      },
+      {
+        name: "La Lupulosa IPA",
+        price: "180"
+      },
+      {
+        name: "Haka Negui Consup Pilsner",
+        price: "200"
+      },
+      {
+        name: "Lindemann's Apple Beer",
+        price: "200"
+      },
+      {
+        name: "Acqua Panna Still",
+        price: "200"
+      },
+      {
+        name: "San Pellegrino",
+        price: "200"
+      },
+      {
+        name: "Agua Franca Still",
+        price: "150"
+      },
+      {
+        name: "Agua Franca Sparklin",
+        price: "150"
+      },
+      {
+        name: "Tonic Water",
+        price: "80"
+      },
+      {
+        name: "Ginger Ale",
+        price: "80"
+      },
+      {
+        name: "Ginger Birra Non Alcoholic",
+        price: "100"
+      },
+      {
+        name: "Coca Cola",
+        price: "80"
+      },
+      {
+        name: "Espresso",
+        price: "90"
+      },
+      {
+        name: "Lungo",
+        price: "100"
+      },
+      {
+        name: "Americano",
+        price: "110"
+      }
+  ],
+
+  bebid:[
+
+  ],
+  
+  tasting: [
+    {
+      name: "PRIMERA ENTRADA",
+      description: "descripción del plato...", 
+      price: "450"
+    }
+    // ... más items
+  ],
+ 
+  cocteles: [
+    {
+      name: "MEZCAL MULE",
+      description: "mezcal, ginger beer...",
+      price: "280"
+    }
+    // ... más items
+  ],
+ 
+  vinos: [
+    {
+      name: "PINOT NOIR",
+      description: "Valle de Guadalupe...",
+      price: "1200",
+      glass: "240"
+    }
+  ]
+ };
+ 
+ // Usa rutas dinámicas
+//  export const routes = {
+//   es: {
+//     carta: 'carta',
+//     cocteles: 'cocteles',
+//     vinos: 'vinos'
+//   },
+//   en: {
+//     carte: 'carte', 
+//     cocktails: 'cocteles',
+//     wines: 'vinos'
+//   }
+//  };
