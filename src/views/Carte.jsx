2747d363c07fb7025de36673d741e9016fda64b0
lisalescano <lisalescano10@gmail.com>
2024-12-14 18:49:31 -0300

    final fix: width carte

--- src/views/Carte.jsx
+++ src/views/Carte.jsx
@@ -23,85 +23,85 @@ const Carte = () => {
   const menuItems = [
     {
       name: "DZIKILPAK",
-      description: "smoked pumpkin seeds - chive oil - heirloom coriander - xoconostle\ncrispy heirloom corn tostada",
+      description: "smoked pumpkin seeds - chive oil - xoconostle\nheirloom coriander - crispy heirloom corn tostada",
       price: "390",
       isVegetarian: true
     },
     {
       name: "VENISON TARTARE",
-      description: "smoked salsa macha - jícama - cotixa cheese - blood tostadas",
+      description: "smoked salsa macha - jícama - cotixa cheese\nblood tostadas",
       price: "490"
     },
     {
       name: "GRILLED MUSHROOM TLAYUDA",
-      description: "mushroom chicharrón - huitlacoche mole - chives - coriander\nblack sesame seed - nasturtium",
+      description: "mushroom chicharrón - huitlacoche mole - chives\ncoriander - black sesame seed - nasturtium",
       price: "550"
     },
     {
       name: "THE WHOLE SQUASH",
-      description: "roasted local squash - squash flowers - grilled squash leaves\ncrispy squash seed",
+      description: "roasted local squash - squash flowers\ngrilled squash leaves - crispy squash seed",
       price: "590",
       isVegetarian: true
     },
     {
       name: "GRILLED BROCCOLINI",
-      description: "herb cesar dressing - fried capers - crispy bread - parmesan crisps\ntoasted seaweed",
+      description: "herb cesar dressing - fried capers - crispy bread\nparmesan crisps - toasted seaweed",
       price: "640",
       isVegetarian: true
     },
     {
       name: "SOFTSHELL CRAB TACO",
-      description: "amaranth and mezcal tempura - hoja santa emulsion -\nfermented habanero & xoconostle salsa",
+      description: "amaranth and mezcal tempura - hoja santa emulsion \nfermented habanero & xoconostle salsa",
       price: "640"
     },
     {
       name: "BONE MARROW",
-      description: "toasted in the woodfire oven - tamarind and pasilla glacé\ngrilled pulque sourdough - serrano salsa",
+      description: "toasted in the woodfire oven - serrano salsa\ngrilled pulque sourdough - tamarind and pasilla glacé",
       price: "720"
     },
     {
       name: "ROASTED PRAWNS",
-      description: "morita chilli - plantain vinegar - prawn butter - manzano chilli ferment",
+      description: "morita chilli - plantain vinegar - prawn butter\nmanzano chilli ferment",
       price: "750"
     },
     {
       name: "CRAB ESQUITES",
-      description: "heirloom corn - crab meat - coconut crab bisque - charred heirloom\ntomato - chives",
+      description: "heirloom corn - crab meat - coconut crab bisque\ncharred heirloom - tomato - chives",
       price: "790"
     },
     {
       name: "BLUEFIN TUNA CRUDO",
-      description: "hibiscus and fermented plum aguachile - morita chili - dulce seaweed\npurple basil",
+      description: "hibiscus and fermented plum aguachile\nmorita chili - dulce seaweed - purple basil",
       price: "840"
     },
     {
       name: "SMOKED TROUT TOSTADA",
-      description: "crispy skin - trout roe - green allspice leaf sauce emulsion\npickled gooseberries - smoked crema",
+      description: "crispy skin - green allspice leaf sauce emulsion\npickled gooseberries - smoked crema -  trout roe",
       price: "840"
     },
     {
       name: "SCALLOP CRUDO",
-      description: "smoked pumpkin seeds - chive oil - heirloom coriander - xoconostle\ncrispy heirloom corn tostada\nadd caviar + 1200",
+      description: "smoked pumpkin seeds - chive oil - xoconostle\nheirloom coriander - crispy heirloom corn tostada\nadd caviar + 1200",
       price: "890"
     },
     {
       name: "STRIPED SEABASS",
-      description: "seared in coconut oil - green mole - local basil - coconut & jicama slaw",
+      description: "seared in coconut oil - green mole - local basil\ncoconut & jicama slaw",
       price: "940"
     },
     {
       name: "MAYAN OCTOPUS",
-      description: "lentil purée - lentil granola - pickled radishes - longaniza\nxcatic & allspice leaf salsa",
+      description: "lentil purée - lentil granola - pickled radishes\nlonganiza - xcatic & allspice leaf salsa",
       price: "980"
     },
     {
       name: "MEXICAN WAGYU TLAYUDA",
-      description: "grilled flank steak - oxtail carnitas - smoked yuca purée - huilacoche mole",
+      description: "grilled flank steak - oxtail carnitas\nsmoked yuca purée - huilacoche mole",
       price: "1180"
     },
     {
       name: "SUCKLING PIG",
-      description: "crispy belly - smoked carnitas - cinnamon leaf oil - rice bean\nsoursop & sea purslane salsa",
+      description: "crispy belly - smoked carnitas - cinnamon leaf oil\nrice bean - soursop & sea purslane salsa",
       price: "1240"
     },
     {
